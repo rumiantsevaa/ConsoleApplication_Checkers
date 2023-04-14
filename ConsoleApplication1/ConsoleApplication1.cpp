95c1595e47344bd458cb49c5669f24a2e7e1b047
arisha <arisha@DESKTOP-8S4ARUB>
2023-04-14 13:46:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int BOARD_SIZE = 8;
+
+void display_board(char board[][BOARD_SIZE]) {
+    
+    cout << "  +---+---+---+---+---+---+---+---+" << endl;
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        cout << i + 1 << " | ";
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            cout << board[i][j] << " | ";
+        }
+        cout << endl << "  +---+---+---+---+---+---+---+---+" << endl;
+    }
+    cout << "    A   B   C   D   E   F   G   H  " << endl;
+}
+
+int main() {
+    char board[BOARD_SIZE][BOARD_SIZE] = {
+        {' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'},
+        {'O', ' ', 'O', ' ', 'O', ' ', 'O', ' '},
+        {' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'X', ' ', 'X', ' ', 'X', ' ', 'X', ' '},
+        {' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'},
+        {'X', ' ', 'X', ' ', 'X', ' ', 'X', ' '}
+    };
+
+    display_board(board);
+
+    return 0;
+}
