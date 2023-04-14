9592a5a30309328499a4009592ac11b97f69b56a
rumiantsevaa <89034072+rumiantsevaa@users.noreply.github.com>
2023-04-14 15:15:10 +0300

    Update ConsoleApplication1.cpp
    Algorithm was added.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 
+//Create a checkers board with pieces already placed on it.
 using namespace std;
 
 const int BOARD_SIZE = 8;
 
 void display_board(char board[][BOARD_SIZE]) {
-    
+
     cout << "  +---+---+---+---+---+---+---+---+" << endl;
     for (int i = 0; i < BOARD_SIZE; i++) {
         cout << i + 1 << " | ";
@@ -30,6 +31,16 @@ int main() {
     };
 
     display_board(board);
+
+//Determine the valid moves for each player's pieces based on the rules of checkers. This includes mandatory jumps and multi-jump sequences.
+
+//Implement code to allow players to input their moves and check if the move is valid.
+
+//Update the game boardand display it after each move.
+
+//Check if a player has won by capturing all of their opponent's pieces or if the game has ended in a draw.
+
+//Implement a turn - based system where players take turns making moves.
 
     return 0;
 }
